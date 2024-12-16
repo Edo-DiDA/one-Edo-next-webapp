85589d4c2712839c6c4f3fc577976760cbc1a380
Eniola Fakeye <eniolafakeye@Eniolas-MacBook-Pro.local>
2024-12-16 12:47:21 +0100

    feat: set up custom story for breadcrumbs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   safelist: ["bg-neutral-100", "bg-primary-700", "bg-primary-400"],
   theme: {
