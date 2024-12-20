dea223e2ac558c28c5cb69f7967d5190c5cbe8d0
Eniola Fakeye <eniolafakeye@Eniolas-MacBook-Pro.local>
2024-12-20 15:10:31 +0100

    fix: services page

--- src/components/contents/service-top-section.tsx
+++ src/components/contents/service-top-section.tsx
@@ -8,8 +8,8 @@ type TopSectionProps = {
 
 const SerivcesTopSection = ({ title, body }: TopSectionProps) => {
   return (
-    <div className="h-auto px-4 md:px-[40px] py-6 lg:h-[344px] md:w-full md:flex md:flex-row md:pt-4 lg:w-[50%] lg:flex lg:pl-[240px] lg:items-center bg-neutral-100 lg:flex-row">
-      <div className="">
+    <div className="h-auto px-4 lg:px-0 md:px-[40px] lg:justify-end py-6 lg:h-[344px] md:w-full md:flex md:flex-row md:pt-4 lg:w-[50%] lg:flex lg:items-center bg-neutral-100 lg:flex-row">
+      <div className="lg:w-[480px]">
         <h2 className="text-mlg font-bold md:text-xxxl lg:text-xxl md:max-w-[528px] lg:max-w-[440px]">
           {title}
         </h2>
